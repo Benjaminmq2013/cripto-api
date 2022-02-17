5327724a5c037ef33cb34cbe50adf290ef3b5c9b
Adriel Benjamín Martínez <61093891+Benjaminmq2013@users.noreply.github.com>
2022-02-17 06:07:15 +0100

    Problems with sizes solved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,6 @@ import "../styles/Header.css"
 const Header = () => {
   return (
     <>
-      
-
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo right">
@@ -26,7 +24,7 @@ const Header = () => {
         </div>
       </nav>
 
-      <h1 className="grey-text text-darken-3">Most Popular Criptos in the world</h1>
+      <h1 id="h1" className="grey-text text-darken-3">Most Popular Criptos in the world</h1>
     </>
   );
 }
